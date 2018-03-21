bf84187410c2c572b3bb19d67ea860ee0396e58d
Derrick Kaffo <dkaffo@toltec-consulting.com>
2018-03-21 13:51:28 +0100

    module Contats

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  contact = {nom:"Paterson", email:"paterson@gmail.com"}
 }
