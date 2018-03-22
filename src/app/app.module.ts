65bfed298b2a06c0d31891d6e6f957f4718b5f8b
Derrick Kaffo <dkaffo@toltec-consulting.com>
2018-03-22 08:18:15 +0100

    contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
+import  { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import {RouterModule, Routes} from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
+const appRoutes:Routes = [
+  {path:'about', component:AboutComponent},
+  {path:'contacts', component:ContactsComponent},
+  {path:'', redirectTo:'/about',pathMatch:'full'}
+  ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactsComponent
+    ContactsComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,  RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
